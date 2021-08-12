67bdb8e847d5edefc63162bace679ccdbb403778
Chow Jia Ying <chowjiaying211@gmail.com>
2021-08-12 16:58:59 +0800

    fix: unlimited rendering with React useEffect

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const App = () => {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    fetch('/api/v1/articles')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [])
   return (
     <div>
       Articles
+      {data.map(({title}) => <div>{title}</div>)}
     </div>
   );
 };
